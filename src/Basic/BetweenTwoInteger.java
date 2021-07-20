412c894887ce65e117dc9afdc0225c1b5857d4ba
creativeGYU <84166310+creativeGYU@users.noreply.github.com>
2021-07-20 23:39:53 +0900

    알고리즘 풀이
    802점

--- src/Basic/BetweenTwoInteger.java
+++ src/Basic/BetweenTwoInteger.java
@@ -3,10 +3,23 @@ package Basic;
 public class BetweenTwoInteger {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		class Solution {
+		    public long solution(int a, int b) {
+		        long answer = 0;
+		        if (a > b) {
+		            for (int i = b; i <= a; i++) {
+		                answer = answer + i;
+		            }
+		        } else {
+		            for (int i = a; i <= b; i++) {
+		                answer = answer + i;
+		            }
+		        }
+		        
+		        return answer;
+		    }
+		}
 	}
-
 }
 
 /* 
